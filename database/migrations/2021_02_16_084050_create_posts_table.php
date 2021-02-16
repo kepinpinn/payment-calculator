5e352f4bcd48a2b53189aa89007cc0a0d648ff04
kepinpin <kelvinlianto@gmail.com>
2021-02-16 16:16:44 +0700

    laravel payment calculator

---
+++ database/migrations/2021_02_16_084050_create_posts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kreditor');
+            $table->date('tanggal');
+            $table->string('items');
+            $table->integer('Amount');
+            $table->decimal('ppn');
+            $table->integer('delivery');
+            $table->integer('total');
+            $table->integer('promo');
+            $table->integer('total_bayar');
+            $table->string('borrower');
+            $table->integer('price_qty');
+            $table->decimal('ppn_borrower');
+            $table->decimal('presentase');
+            $table->integer('promo_borrower');
+            $table->integer('total_bayar_borrower');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
