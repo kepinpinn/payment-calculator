a0dbd75c3ff00d7164891f18cca41106a7d932c6
kepinpin <kelvinlianto@gmail.com>
2021-06-04 11:19:27 +0700

    index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -41,7 +41,7 @@
 
     <div class="card">
         <div class="card-header">
-            <h2>HOETANG</h2>
+            <h2>HUTANG</h2>
             <div class="float-right">
                 <a class="btn btn-secondary" href="{{ route('payments.create') }}"> Bayar Hutang</a>
             </div>
